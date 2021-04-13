0b6e47807084e18629818d2f3234e1e021dd195a
InFinity_54 <corentinkreicher@gmail.com>
2021-04-13 16:22:12 +0200

    Retrait de la posssibilité de sélectionner des champions déjà sélectionnés dans d'autres listes

--- public/assets/js/pages/pool.edit.js
+++ public/assets/js/pages/pool.edit.js
@@ -13,3 +13,35 @@ $("#pool-form").validate({
     errorClass: "is-invalid",
     validClass: "is-valid"
 });
+
+const $selects = $(".poolSelect");
+
+function disableAlreadySelectedChamps() {
+    const selectedValue = [];
+
+    $selects.find(':selected').filter(function(idx, el) {
+        return $(el).attr('value');
+    }).each(function(idx, el) {
+        selectedValue.push($(el).attr('value'));
+    });
+
+    $selects.find('option').each(function(idx, option) {
+        if (selectedValue.indexOf($(option).attr('value')) > -1) {
+            if ($(option).is(':checked')) {
+                return;
+            } else {
+                $(this).attr('disabled', true);
+            }
+        } else {
+            $(this).attr('disabled', false);
+        }
+    });
+
+    $('.selectpicker').selectpicker('refresh');
+}
+
+$selects.on('change', function (evt) {
+    disableAlreadySelectedChamps();
+});
+
+disableAlreadySelectedChamps();
